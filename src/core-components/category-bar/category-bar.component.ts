8f4326b59567e8d95e571048cd12f607cf19d17c
mo <vitch.michhh@gmail.com>
2024-09-05 19:04:20 +0300

    initate structure

---
+++ src/core-components/category-bar/category-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './category-bar.component.html',
+  styleUrl: './category-bar.component.scss'
+})
+export class CategoryBarComponent {
+
+}
